f4583c0e0723b9503ac13392472080b40ddf52a9
ruslan <71116615+kerakelv@users.noreply.github.com>
2022-09-14 11:12:32 +0300

    Create kerakelv1.sh

---
+++ kerakelv1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function colors {
+  GREEN="\e[32m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+colors
+
+echo -e "${GREEN}╔╗╔═╗░░░░░░░╔╗░░░░╔╗░░░░${NORMAL}"
+echo -e "${GREEN}║║║╔╝░░░░░░░║║░░░░║║░░░░${NORMAL}"
+echo -e "${GREEN}║╚╝╝╔══╦═╦══╣║╔╦══╣║╔╗╔╗${NORMAL}"
+echo -e "${GREEN}║╔╗║║║═╣╔╣╔╗║╚╝╣║═╣║║╚╝║${NORMAL}"
+echo -e "${GREEN}║║║╚╣║═╣║║╔╗║╔╗╣║═╣╚╬╗╔╝${NORMAL}"
+echo -e "${GREEN}╚╝╚═╩══╩╝╚╝╚╩╝╚╩══╩═╝╚╝░${NORMAL}"
+
